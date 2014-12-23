ec1b273c962dd15c0c8f43c35abe21f48f536644
Sherief Gharraph <sgharraph@pillartechnology.com>
2014-12-22 21:59:36 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookItEasy::Application.config.secret_token = '761ca91c7bc21699995dd14717561ef0cfc8b8628f6a77668d480b890a5651deca5877cf00ef361185a478ddb07767abcf2398df5c4079bafbd2f305bfe04107'
